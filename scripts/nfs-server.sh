3c3f3dd8bb40d440e9c99b43afeba2ac5039c9e0
Deepak Nadig <admin@deepaknadig.com>
2020-03-22 17:42:50 -0500

    Updated NFS script.

--- scripts/nfs-server.sh
+++ scripts/nfs-server.sh
@@ -1,18 +1,41 @@
 #!/bin/bash
 
-NFS_STORE=/srv/nfs/kube/mongo
-LINE='$NFS_STORE	*(rw,sync,no_subtree_check,no_root_squash,no_all_squash)'
-FILE='/etc/exports'
+NFS_STORE="/srv/nfs/kube"
+LINE="$NFS_STORE	*(rw,sync,no_subtree_check,no_root_squash,no_all_squash)"
+FILE="/etc/exports"
 
 sudo apt install nfs-kernel-server
-sudo mkdir -p $NFS_STORE
 
-grep -qF -- "$LINE" "$FILE" || sudo echo "$LINE" >> "$FILE"
 
+if [ ! -d $NFS_STORE ]; then
+    printf '%s\n' --------------------
+    printf "Creating directories...\n"
+    printf '%s\n' --------------------
+    sudo mkdir -p $NFS_STORE
+    ls -R /srv
+    printf '%s\n' --------------------
+    printf "Directories created.\n"
+    printf '%s\n' --------------------
+fi
+
+printf '%s\n' --------------------
+printf "Configuring NFS service...\n"
+printf '%s\n' --------------------
+grep -qF -- "$LINE" "$FILE" || echo $LINE | sudo tee -a $FILE
 sudo chown nobody:nogroup $NFS_STORE
 sudo exportfs -rav
 sudo exportfs -v
+printf '%s\n' --------------------
+printf "Done.\n"
+printf '%s\n' --------------------
 
+printf '%s\n' --------------------
+printf "Starting NFS service...\n"
+printf '%s\n' --------------------
 sudo systemctl daemon-reload
 sudo systemctl start nfs-server.service
 sudo systemctl restart nfs-server.service
+sudo systemctl status nfs-server.service
+printf '%s\n' --------------------
+printf "Done.\n"
+printf '%s\n' --------------------
