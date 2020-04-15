603c613f7be63aa49d7313c0828103542a1b43fd
Sara <ea.sara@ymail.com>
2020-04-14 20:54:47 -0500

    Add required file for image_processing.py

---
+++ app/parsers.py
@@ -0,0 +1,9 @@
+import werkzeug
+from flask_restx import reqparse
+
+file_upload = reqparse.RequestParser()
+file_upload.add_argument('file',
+                         type=werkzeug.datastructures.FileStorage,
+                         location='files',
+                         required=True,
+                         help='Image')
