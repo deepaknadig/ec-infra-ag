2012faa0a809636c59ac1e8a59d42db2b23b4b38
Sara <ea.sara@ymail.com>
2020-04-14 20:56:37 -0500

    Added image processing api

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@ from flask_restx import Api
 
 from .device import api as device_api
 from .measurement import api as measurement_api
+from .image_processing import api as image_processing_api
 
 from prometheus_flask_exporter import PrometheusMetrics
 
@@ -25,6 +26,7 @@ metrics = PrometheusMetrics(app=None)
 
 api.add_namespace(device_api)
 api.add_namespace(measurement_api)
+api.add_namespace(image_processing_api)
 
 app.register_blueprint(blueprint)
 metrics.init_app(app)
