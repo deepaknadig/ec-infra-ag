ceb5eb9e86278a61e1afadf3c2a265f0325091cd
Deepak Nadig <admin@deepaknadig.com>
2020-05-11 15:44:32 -0500

    Added task queue.

--- app/__init__.py
+++ app/__init__.py
@@ -7,10 +7,17 @@ from .image_processing import api as image_processing_api
 
 from prometheus_flask_exporter import PrometheusMetrics
 
+from celery import Celery
+
 app = Flask(__name__)
 app.config.SWAGGER_UI_DOC_EXPANSION = 'list'
 app.config.SWAGGER_UI_REQUEST_DURATION = True
 app.config.RESTX_MASK_SWAGGER = False
+
+app.config.from_object(app.config)
+
+# Celery set up
+client = Celery(app.name, broker=app.config['CELERY_BROKER_URL'])
 
 blueprint = Blueprint('api', __name__, url_prefix='/api/v1')
 
---
+++ app/config.py
@@ -0,0 +1,2 @@
+CELERY_BROKER_URL = 'redis://redis-flask-service:6379/0'
+#CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
